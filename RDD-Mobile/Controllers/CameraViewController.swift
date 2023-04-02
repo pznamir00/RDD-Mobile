d3f7061993cc202639b183bd64e5f263c8449349
pznamir00 <pznamir00@gmail.com>
2023-04-02 15:56:32 +0200

    perf(): add needed controls

--- RDD-Mobile/Controllers/CameraViewController.swift
+++ RDD-Mobile/Controllers/CameraViewController.swift
@@ -8,22 +8,11 @@
 import UIKit
 
 class CameraViewController: UIViewController {
-
+    @IBOutlet weak var recordButton: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         self.title = "Camera"
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
--- RDD-Mobile/Controllers/OverviewViewController.swift
+++ RDD-Mobile/Controllers/OverviewViewController.swift
@@ -6,24 +6,19 @@
 //
 
 import UIKit
+import MapKit
 
 class OverviewViewController: UIViewController {
-
+    @IBOutlet weak var locationAccessIconRef: UIImageView!
+    @IBOutlet weak var cameraAccessIconRef: UIImageView!
+    @IBOutlet weak var internetConnectionIconRef: UIImageView!
+    @IBOutlet weak var pointsCounter: UILabel!
+    @IBOutlet weak var map: MKMapView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        self.title = "Overview"
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
